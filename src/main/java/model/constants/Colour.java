09438f5577178dea5b94112b4eb2e90437d63fc8
anastasiya.zabrodskaya <anastasiya.zabrodskaya94@mail.ru>
2026-05-24 14:15:47 +0300

    Fix

--- src/main/java/model/constants/Colour.java
+++ src/main/java/model/constants/Colour.java
@@ -2,5 +2,5 @@ package model.constants;
 
 public class Colour {
     public static final String RED = "red";
-    public static final String Green = "green";
+    public static final String GREEN = "green";
 }
